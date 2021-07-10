c4ff8c978dfb045ebcc3a9ecca56ec1af5acd2b0
priyankanigam8 <86011052+priyankanigam8@users.noreply.github.com>
2021-07-10 04:41:03 -0600

    Add files via upload

---
+++ max-min.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"Enter the two numbers"<<endl;
+    //endl represents the end of line
+    cin>>a;
+    cin>>b;
+    if (a>b)
+    {
+        cout<<"a is maximum"<<endl;
+
+    }
+    else
+    {
+        cout<<"b is maximum"<<endl;
+    }
+    return 0;
+    
+    
+}
